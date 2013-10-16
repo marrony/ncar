cd5d14b338d6c1e102c09baf5d3a1cc325149a83
Lohandus <lohandus@gmail.com>
2013-10-16 01:29:00 -0300

    Commit inicial

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour {
+	
+	public WheelCollider FrontLeftWheel;
+	public WheelCollider FrontRightWheel;
+	public WheelCollider RearLeftWheel;
+	public WheelCollider RearRightWheel;
+	
+	public float[] GearRatio;
+	public int CurrentGear = 0;
+	
+	public float EngineTorque = 600.0f;
+	public float MaxEngineRPM = 3000.0f;
+	public float MinEngineRPM = 1000.0f;
+		
+	private float EngineRPM = 0.0f;
+
+	void Start () {
+		//Transform cg = transform.FindChild("CG");
+		rigidbody.centerOfMass = new Vector3(0, -0.7f, 0);
+	}
+
+	void Update () {
+		rigidbody.drag = rigidbody.velocity.magnitude / 250;
+		
+		EngineRPM = (FrontLeftWheel.rpm + FrontRightWheel.rpm)/2 * GearRatio[CurrentGear];
+		ShiftGears();
+	
+		/*
+		audio.pitch = Mathf.Abs(EngineRPM / MaxEngineRPM) + 1.0 ;
+		if ( audio.pitch > 2.0 ) 
+			audio.pitch = 2.0;
+		*/
+
+		var motorTorque = EngineTorque / GearRatio[CurrentGear] * Input.GetAxis("Vertical");
+		RearLeftWheel.motorTorque = -motorTorque;
+		RearRightWheel.motorTorque = -motorTorque;
+
+		FrontLeftWheel.steerAngle = 20 * Input.GetAxis("Horizontal");
+		FrontRightWheel.steerAngle = 20 * Input.GetAxis("Horizontal");
+	}
+	
+	private void ShiftGears() {
+		if ( EngineRPM >= MaxEngineRPM ) {
+			int AppropriateGear = CurrentGear;
+			
+			for (int i = 0; i < GearRatio.Length; i ++ ) {
+				if ( FrontLeftWheel.rpm * GearRatio[i] < MaxEngineRPM ) {
+					AppropriateGear = i;
+					break;
+				}
+			}
+			
+			CurrentGear = AppropriateGear;
+		}
+		
+		if ( EngineRPM <= MinEngineRPM ) {
+			int AppropriateGear = CurrentGear;
+			
+			for (int j = GearRatio.Length-1; j >= 0; j -- ) {
+				if ( FrontLeftWheel.rpm * GearRatio[j] > MinEngineRPM ) {
+					AppropriateGear = j;
+					break;
+				}
+			}
+			
+			CurrentGear = AppropriateGear;
+		}
+	}
+	
+	void OnGUI() {
+		GUI.Label(new Rect(10, 10, 100, 20), "KM/h: " + (rigidbody.velocity.magnitude * 3.6f));
+	}
+	
+} 
---
+++ Assets/Scripts/WheelAlignment.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelAlignment : MonoBehaviour {
+
+	public WheelCollider CorrespondingCollider;
+	public GameObject SlipPrefab;
+	private float RotationValue = 0.0f;
+	
+
+	void Update () {
+		RaycastHit hit;
+		Vector3 ColliderCenterPoint = CorrespondingCollider.transform.TransformPoint(CorrespondingCollider.center);
+		
+		// now cast a ray out from the wheel collider's center the distance of the suspension, if it hit something, then use the "hit"
+		// variable's data to find where the wheel hit, if it didn't, then se tthe wheel to be fully extended along the suspension.
+		if ( Physics.Raycast(ColliderCenterPoint, -CorrespondingCollider.transform.up, out hit, CorrespondingCollider.suspensionDistance + CorrespondingCollider.radius)) {
+			transform.position = hit.point + (CorrespondingCollider.transform.up * CorrespondingCollider.radius);
+		}else{
+			transform.position = ColliderCenterPoint - (CorrespondingCollider.transform.up * CorrespondingCollider.suspensionDistance);
+		}
+		
+		// now set the wheel rotation to the rotation of the collider combined with a new rotation value. This new value
+		// is the rotation around the axle, and the rotation from steering input.
+		transform.rotation = CorrespondingCollider.transform.rotation * 
+			Quaternion.Euler( RotationValue, CorrespondingCollider.steerAngle, 0 ) *
+			Quaternion.Euler(0, 90, 0);
+		
+		// increase the rotation value by the rotation speed (in degrees per second)
+		RotationValue += CorrespondingCollider.rpm * ( 360/60 ) * Time.deltaTime;
+
+		// define a wheelhit object, this stores all of the data from the wheel collider and will allow us to determine
+		// the slip of the tire.
+		WheelHit CorrespondingGroundHit ;
+		CorrespondingCollider.GetGroundHit(out CorrespondingGroundHit );
+		
+		// if the slip of the tire is greater than 2.0, and the slip prefab exists, create an instance of it on the ground at
+		// a zero rotation.
+		if ( Mathf.Abs( CorrespondingGroundHit.sidewaysSlip ) > 2.0f ) {
+			if ( SlipPrefab ) {
+				Instantiate( SlipPrefab, CorrespondingGroundHit.point, Quaternion.identity );
+			}
+		}
+		
+	}
+}
