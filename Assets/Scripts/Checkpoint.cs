5661c7266f6912ee3c9a8cbda8bb70eeae489244
Lohandus <lohandus@gmail.com>
2013-10-30 01:27:17 -0200

    Rascunho do checkpoint

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+
+	void Start () 
+	{
+		gameObject.AddComponent<BoxCollider>();
+		collider.isTrigger = true;
+	}
+
+	void OnTriggerEnter (Collider other) 
+	{
+		Debug.Log("Checkpoint!");
+	}
+	
+	void OnDrawGizmos()
+	{
+		Gizmos.matrix = transform.localToWorldMatrix;
+		
+		Gizmos.color = new Color(0, 0, 1, .5f);
+		Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+		
+		Gizmos.color = new Color(0, 0, 1, .1f);
+		Gizmos.DrawCube(Vector3.zero, Vector3.one);
+	}
+
+}
