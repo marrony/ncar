e50148f41e1a1c2986fda8bc50be09fc867851b5
Lohandus <lohandus@gmail.com>
2013-10-30 01:25:07 -0200

    Removendo o carro da cena e instanciando em runtime. Agora, qualquer alteração no carro não vai afetar a cena.

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour {
+	
+	public SmoothFollow mainCamera;
+	public GameObject maverick;
+
+	void Start () {
+		GameObject newMaverick = Instantiate(maverick, 
+			new Vector3(200, 2, 260), 
+			Quaternion.Euler(0, 230, 0)) as GameObject;
+		
+		mainCamera.target = newMaverick.transform;
+	}
+
+}
