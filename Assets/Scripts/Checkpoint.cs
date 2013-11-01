c3f57cc03483116117eb193532ba0b335596187e
Lohandus <lohandus@gmail.com>
2013-10-31 23:42:11 -0200

    Contando as voltas

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -1,17 +1,30 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class Checkpoint : MonoBehaviour {
-
+	
+	public Checkpoint next;
+	
+	public event OnCheckpointEnterEventHandler OnCheckpointEnter = delegate {};
+	
+	private HashSet<GameObject> waitingFor = new HashSet<GameObject>();
+	
 	void Start () 
 	{
 		gameObject.AddComponent<BoxCollider>();
 		collider.isTrigger = true;
 	}
 
-	void OnTriggerEnter (Collider other) 
+	void OnTriggerEnter (Collider collider) 
 	{
-		Debug.Log("Checkpoint!");
+		GameObject gameObject = collider.transform.root.gameObject;
+		
+		if (!waitingFor.Contains(gameObject))
+			return;
+		
+		waitingFor.Remove(gameObject);
+		next.WaitFor(gameObject);
+		OnCheckpointEnter(gameObject);
 	}
 	
 	void OnDrawGizmos()
@@ -24,5 +37,12 @@ public class Checkpoint : MonoBehaviour {
 		Gizmos.color = new Color(0, 0, 1, .1f);
 		Gizmos.DrawCube(Vector3.zero, Vector3.one);
 	}
+	
+	public void WaitFor(GameObject gameObject) 
+	{
+		waitingFor.Add(gameObject);
+	}
 
 }
+
+public delegate void OnCheckpointEnterEventHandler(GameObject gameObject);
--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -5,13 +5,27 @@ public class GameMaster : MonoBehaviour {
 	
 	public SmoothFollow mainCamera;
 	public GameObject maverick;
+	public Checkpoint startLine;
+	
+	private int lap = 0;
 
 	void Start () {
-		GameObject newMaverick = Instantiate(maverick, 
+		GameObject playerCar = Instantiate(maverick, 
 			new Vector3(200, 2, 260), 
 			Quaternion.Euler(0, 230, 0)) as GameObject;
 		
-		mainCamera.target = newMaverick.transform;
+		mainCamera.target = playerCar.transform;
+		
+		startLine.WaitFor(playerCar);
+		startLine.OnCheckpointEnter += (gameObject) => { 
+			if (gameObject == playerCar)
+				lap++; 
+		};
+	}
+	
+	void OnGUI() 
+	{
+		GUI.Label(new Rect(Screen.width/2, 10, 80, 20), "Lap " + lap);
 	}
 
 }
