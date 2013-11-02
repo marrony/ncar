2f1035762af9d50725f2bd89363bb5bdf1aea2e5
Marrony Neris <neris.marrony@gmail.com>
2013-11-02 00:31:44 -0200

    adicionado os waypoints na pista

---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoints : MonoBehaviour {
+	
+	public Waypoints[] next;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnDrawGizmos() {
+		//Gizmos.matrix = transform.localToWorldMatrix;
+		
+		Gizmos.color = new Color(1, 0, 0, .5f);
+		Gizmos.DrawWireCube(transform.position, Vector3.one);
+		
+		foreach(Waypoints ways in next) {
+			Gizmos.DrawLine(transform.position, ways.transform.position);
+		}
+	}
+}
