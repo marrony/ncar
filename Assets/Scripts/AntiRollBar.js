01ba09b04212be0f6e177e62dfd7b35f2586d4fd
Bruno Artico <brunorogerio@yahoo.com.br>
2013-12-28 18:38:36 -0200

    Possivel fix pra issue #9

--- Assets/Scripts/AntiRollBar.js
+++ Assets/Scripts/AntiRollBar.js
@@ -4,7 +4,7 @@ var AntiRoll = 5000.0;
 
 function FixedUpdate () 
 { 
-    var hit : WheelHit; 
+    var hit : WheelHit;
     var travelL = 1.0; 
     var travelR = 1.0;
 
@@ -18,10 +18,26 @@ function FixedUpdate ()
         travelR = (-WheelR.transform.InverseTransformPoint(hit.point).y - WheelR.radius) 
                   / WheelR.suspensionDistance;
 
-    var antiRollForce = (travelL - travelR) * AntiRoll;
+    var antiRollForce = (travelL - travelR) * AntiRoll;					
+	
+	if(rayCastWillNotHitGround(transform) || rayCastWillNotHitGround(WheelL.transform) || rayCastWillNotHitGround(WheelR.transform))
+		return;	
+			
+    if (groundedL)
+    	rigidbody.AddForceAtPosition(WheelL.transform.up * -antiRollForce, WheelL.transform.position); 	
+    
+    if (groundedR)
+        rigidbody.AddForceAtPosition(WheelR.transform.up * antiRollForce, WheelR.transform.position);     
+}
 
-    if (groundedL) 
-        rigidbody.AddForceAtPosition(WheelL.transform.up * -antiRollForce, WheelL.transform.position); 
-    if (groundedR) 
-        rigidbody.AddForceAtPosition(WheelR.transform.up * antiRollForce, WheelR.transform.position); 
+function rayCastWillNotHitGround(transformToCheck){
+	var hits : RaycastHit[];
+	hits = Physics.RaycastAll (transformToCheck.position, transformToCheck.up * -1, 1.0);	
+	for (var i = 0;i < hits.Length; i++) {
+		var hit : RaycastHit = hits[i];
+		if(!hit.collider.GetType().Name.Equals("TerrainCollider"))			
+			return true;
+	}
+	
+	return false;
 }
